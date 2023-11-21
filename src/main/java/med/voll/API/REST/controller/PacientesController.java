5ca8417ff84035ebded45a303cd1cbe3b119d770
Caio Henrique Mafra <caiomafra10@gmail.com>
2023-11-20 22:18:11 -0300

    Requisições de listagem de dados com paginação, ordenação e limitação. (Médicos e Pacientes)

--- src/main/java/med/voll/API/REST/controller/PacientesController.java
+++ src/main/java/med/voll/API/REST/controller/PacientesController.java
@@ -2,14 +2,15 @@ package med.voll.API.REST.controller;
 
 import jakarta.validation.Valid;
 import med.voll.API.REST.paciente.DadosCadastroPaciente;
+import med.voll.API.REST.paciente.DadosListagemPaciente;
 import med.voll.API.REST.paciente.Paciente;
 import med.voll.API.REST.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/pacientes")
@@ -22,5 +23,10 @@ public class PacientesController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
         repository.save(new Paciente(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemPaciente> listar(@PageableDefault(size = 10, sort = "nome") Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemPaciente::new);
     }
 }
